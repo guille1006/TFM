fa5fd5608d15159830b46ee6e19d41d0a008c529
guille1006 <guilleia1006@gmail.com>
2025-07-03 22:50:33 +0200

    clase

---
+++ Idealista/utils.py
@@ -0,0 +1,68 @@
+import requests
+import base64
+from datetime import datetime, timedelta
+
+
+
+
+#------------------------------------------------------------------------------------------------------------
+# Pedir a la API que me devuelva un token 
+def idealista_token(api_key, secret):
+    """ Request a token from OAuth authentification
+
+    Parameters
+    ----------
+    api_key(str): Api key
+    secret(str): secreto
+
+    Returns
+    -------
+    """
+    real_credentials = api_key+":"+secret
+    base64_bytes = base64.b64encode(real_credentials.encode("ascii"))
+    credentials = base64_bytes.decode("ascii")
+
+
+    req = requests.post(
+        "https://api.idealista.com/oauth/token",
+        "grant_type=client_credentials&scope=read",
+        headers={
+            "Authorization": f"Basic {credentials}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    ).json()
+    return req["access_token"], datetime.now() + timedelta(seconds=req["expires_in"])
+
+#--------------------------------------------------------------------------------------------------------------
+# Realiza una consulta a la API de idealista
+def query(token, request):
+    """Realiza una consulta a la API de idealista
+    Parameter
+    ---------
+    token: token recibido por la API de outhenticator
+    request: diccionario con los datos de la solicitud
+
+    Returns
+    -------
+    Devuelve la respuesta de la API
+    """
+
+    response = request.post(
+            "https://api.idealista.com/3.5/es/search",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "User-Agent": "curl/8.3.0"
+            },
+            data=request,
+        )
+    return response.json()
+
+
+
+
+filter_request = {
+    "country": "es",
+    "operation": "sale",
+    "propertyType": "homes",
+    
+}
